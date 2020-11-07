1fd7558d7c08afe43250cec186253c358cf79dec
Taaku18 <45324516+Taaku18@users.noreply.github.com>
2020-11-08 06:35:57 +0800

    Add start/end

--- get_out.cpp
+++ get_out.cpp
@@ -37,6 +37,8 @@ private:
 public:
     int h;
     int w;
+    int startPos;
+    int endPos;
 
     Maze(int height, int width){
         h = height;
@@ -45,12 +47,12 @@ public:
         W = (2 * w) + 1;
     };
 
-    void generateNewMaze(){
+    void generateNewMaze() {
         grid.resize(W, std::vector<int>(H, 1));
         std::srand(std::time(nullptr));
-        int currentRow = std::rand() % SIZE + 1;
+        int currentRow = std::rand() % H + 1;
         currentRow = (currentRow % 2 == 1 ? currentRow : currentRow - 1);
-        int currentCol = std::rand() % SIZE + 1;
+        int currentCol = std::rand() % W + 1;
         currentCol = (currentCol % 2 == 1 ? currentCol : currentCol - 1);
         grid[currentRow][currentCol] = 0;
         int visited = 1;
@@ -64,11 +66,11 @@ public:
                 currentCol = x[index][1];
                 continue;
             }
-            for (auto & neighbor : neighbors) {
+            for (auto &neighbor : neighbors) {
                 int checkRow = neighbor[0];
                 int checkCol = neighbor[1];
                 if (grid[checkRow][checkCol] > 0) {
-                    grid[(checkRow + currentRow)/2][(checkCol + currentCol)/2] = 0;
+                    grid[(checkRow + currentRow) / 2][(checkCol + currentCol) / 2] = 0;
                     grid[checkRow][checkCol] = 0;
                     visited++;
                     currentRow = checkRow;
@@ -77,7 +79,23 @@ public:
                 }
             }
         }
-    };
+        startPos = -1;
+        while (startPos == -1) {
+            int index = std::rand() % H;
+            if (grid[index][1] == 0) {
+                startPos = index;
+                grid[index][0] = 0;
+            }
+        }
+        endPos = -1;
+        while (endPos == -1) {
+            int index = std::rand() % H;
+            if (grid[index][W-2] == 0) {
+                endPos = index;
+                grid[index][W-1] = 0;
+            }
+        }
+    }
 
     void printMaze() {
         for (auto &i : grid) {
