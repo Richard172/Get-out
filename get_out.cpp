4c41774fc327be9660f8029e3ac3aa70443741e8
Richard <jiang1774624096@gmail.com>
2020-11-07 13:37:45 -0600

    first commit

---
+++ get_out.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#define SIZE 26
+
+class Node
+{
+public:
+	Node* next;
+	Node* left;
+	Node* right;
+	Node* up;
+	Node* down;
+	bool left;
+	bool right;
+	bool up;
+	bool down;
+};
+
+void print_maze(Node* first)
+{
+	
+}
+
+int main()
+{
+	
+}
