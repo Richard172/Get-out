3ae1a3f22f02e06b6a49a0a788d58097555df0c1
Richard <jiang1774624096@gmail.com>
2020-11-07 14:30:44 -0600

    changes made

--- random.cpp
+++ random.cpp
@@ -7,7 +7,14 @@ int randBetweenTwoNumber(int firstNum, int secondNum)
 	// a smaller number between the two
 	int smallerNum = (firstNum > secondNum ? secondNum : firstNum);
 	int difference = std::abs(firstNum - secondNum);
-	return rand() % (difference + 1) + smallerNum;
+	return std::rand() % (difference + 1) + smallerNum;
+}
+
+int oddrand(int x)
+{
+	int moduloOperant = x;
+	int randomNum = std::rand() % moduloOperant + 1;
+	return (randomNum % 2 == 1 ? randomNum : randomNum - 1);
 }
 
 int main()
@@ -16,5 +23,5 @@ int main()
 	// generate a random seed based on what time the pc is (in sec)
 	std::srand(std::time(NULL));
 	
-	std::cout << randBetweenTwoNumber(3, 6);
+	std::cout << oddrand(5);
 }
